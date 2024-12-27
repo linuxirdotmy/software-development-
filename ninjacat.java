94334780b692fcb62e423845f9e35e26a1238117
linuxirdotmy <linuxirdotmy@gmail.com>
2024-12-27 16:48:14 +0800

    Update ninjacat.java

--- ninjacat.java
+++ ninjacat.java
@@ -1,25 +1,57 @@
-// This is ninjacat
+// Critter class extension for a critter called a 'NinjaCat'
+// Author: Muhamad Nazir
 
-java
-import java.awt.*;
+import java.awt.Color;
+
 public class NinjaCat extends Critter {
-    public NinjaCat() {
-        // Custom initialization if needed
+    private int infections;
+    public NinjaCat(){
+        infections=0;
     }
-    public Action getMove(CritterInfo info) {
-        if (info.frontThreat()) {
-            return Action.INFECT;
-        } else if (info.getFront() == Neighbor.EMPTY) {
-            return Action.HOP;
-        } else {
-            return Action.RIGHT;
+    
+    //display number of infections
+    public String toString(){
+        return "K: "+infections+"";
+    }
+    
+    //color is based on how many infections it has done
+    public Color getColor(){
+        if(infections==0){
+           return Color.YELLOW; 
+        }
+        else if(infections==1){
+            return Color.cyan;
+        }
+        else if (infections==2){
+            return Color.MAGENTA;
+        }
+        else if (infections==3){
+            return Color.orange;
+        }
+        else if (infections==4){
+            return Color.PINK;
+        }
+        else if (infections==5){
+            return Color.GREEN;
+        }
+        return Color.RED;
+        
+    }
+    
+    //same as giant
+    public Critter.Action getMove(CritterInfo info){
+       
+      
+        if (info.getFront()==Critter.Neighbor.OTHER){
+            infections++;
+            return Critter.Action.INFECT;
+      
+        }
+       else if(info.getFront()==Critter.Neighbor.EMPTY){
+            return Critter.Action.HOP;
+        }
+        else{
+            return Critter.Action.RIGHT;
         }
     }
-    public Color getColor() {
-        return Color.MAGENTA;
-    }
-    public String toString() {
-        return "NC";
-    }
 }
-
