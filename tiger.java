94c1f360bdaf0ac0a9ef9b304bda119b3c64dda0
linuxirdotmy <linuxirdotmy@gmail.com>
2024-12-27 17:01:09 +0800

    Update tiger.java

--- tiger.java
+++ tiger.java
@@ -1,52 +1,60 @@
 // Tiger critter by Muhamad Nazir
 
-import java.awt.Color;
+import java.awt.*;
+import java.util.*;
+
 
 public class Tiger extends Critter {
-    private int moves;
-    private Color currentColor;
- 
+    private int colorMoves;
+    Color tigerColor;
+    Random rand = new Random();
+
     public Tiger(){
-        moves=0;
-        
+        colorMoves=0;//1,2,3
+        getColor();
     }
-    
-    public Color getColor(){
-        int rand;
-        if(moves%3==0){
-            rand=(int) (Math.random()*2);
-            if(rand==0){
-                currentColor=Color.RED;
+
+    public Color getColor() {
+        //Randomly picks one of three colors (Color.RED, Color.GREEN, Color.BLUE) and uses that color for three moves,
+        // then randomly picks one of those colors again for the next three moves,
+        // then randomly picks another one of those colors for the next three moves, and so on.
+        if (colorMoves%3==0){ // set new color
+            int x=0;
+            while (x==0){
+                int i=rand.nextInt(3); //0.Red 1.Green 2.Black
+                if (i==0 && this.tigerColor!=Color.RED){
+                    this.tigerColor= Color.RED;
+                    x++;
+                } if (i==1 && tigerColor!=Color.GREEN){
+                    this.tigerColor=Color.GREEN;
+                    x++;
+                } if (i==2 && tigerColor!=Color.BLUE){
+                    this.tigerColor=Color.BLUE;
+                    x++;
+                }
             }
-            if(rand==1){
-               currentColor=Color.BLUE; 
-            }
-            if(rand==2){
-                currentColor=Color.GREEN;
-            }
+
         }
-        return currentColor;
-        
+        return tigerColor;
     }
-    
-    public String toString(){
+
+    public String toString() {
         return "TGR";
     }
-    
-    public Action getMove(CritterInfo info){
-        moves++;
-        if(info.getFront()==Neighbor.OTHER){
+
+    public Action getMove(CritterInfo info) {
+        //always infect if an enemy is in front,
+        // otherwise if a wall is in front or to the right, then turn left,
+        // otherwise if a fellow Tiger is in front, then turn right, otherwise hop.
+        colorMoves++;
+        if (info.getFront()==Neighbor.OTHER){
             return Action.INFECT;
-        }
-        else if(info.getFront()==Neighbor.WALL||info.getRight()==Neighbor.WALL){
+        } else if (info.getFront()==Neighbor.WALL||info.getRight()==Neighbor.WALL){
             return Action.LEFT;
-        }
-        else if(info.getFront()==Neighbor.SAME){
+        } else if (info.getFront()==Neighbor.SAME){
             return Action.RIGHT;
-        }
-        else{
+        } else {
             return Action.HOP;
         }
     }
-   }
-
+}
