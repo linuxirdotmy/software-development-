e79cdd684dde5a5456c66fa887ee95f2cc301c6d
linuxirdotmy <linuxirdotmy@gmail.com>
2024-12-27 16:59:41 +0800

    Update Whitetiger.java

--- Whitetiger.java
+++ Whitetiger.java
@@ -1,24 +1,38 @@
-//this is whitetiger class
+//Whitetiger critters by Muhamad Nazir
 
-java
 import java.awt.*;
+
 public class WhiteTiger extends Tiger {
-    private boolean hasInfected;
-    public WhiteTiger() {
-        super();
-        this.hasInfected = false;
+    boolean hasInfected;
+
+    public WhiteTiger(){
+        hasInfected=false;
     }
 
-    public Action getMove(CritterInfo info) {
-        if (info.getFront() == Neighbor.OTHER) {
-            hasInfected = true;
-        }
-        return super.getMove(info);
-    }
+
     public Color getColor() {
+        //Always Color.WHITE.
         return Color.WHITE;
     }
+
+
     public String toString() {
-        return hasInfected ? "TGR" : "tgr";
+        //"tgr" if it hasn’t infected another Critter yet, “TGR” if it has infected.
+        if (hasInfected){
+            return super.toString();
+        } else {
+            return "tgr";
+        }
+    }
+
+
+    public Action getMove(CritterInfo info) {
+        //Same as a Tiger.
+        // Note: you’ll have to override this method to figure out if it has infected another Critter.
+        if (info.getFront()==Neighbor.OTHER){
+            hasInfected=true;
+        }
+        return super.getMove(info);
+
     }
 }
